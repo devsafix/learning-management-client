537746009e72dd73fc69763d04e467c02e6639ee
devsafix <devsafix@gmail.com>
2025-09-27 12:12:06 +0600

    Add: add marquee component in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/module/home/Banner";
+import Marquee from "@/components/module/home/Marquee";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Marquee />
     </div>
   );
 }
